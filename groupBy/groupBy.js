f33fbb28f65b5768aceb6dfeb3a13b8e7decc84b
Dileep A <99489054+dileepsa@users.noreply.github.com>
2022-04-05 15:57:37 +0530

    Extracted 'groupElement' function. Changed function names.

--- groupBy/groupBy.js
+++ groupBy/groupBy.js
@@ -1,7 +1,3 @@
-const areLengthsUndefined = function (element1, element2) {
-  return element1.length === undefined && element2.length === undefined;
-};
-
 const areArraysEqual = function (array1, array2) {
   if (array1.length !== array2.length) {
     return false;
@@ -16,17 +12,17 @@ const areArraysEqual = function (array1, array2) {
   return true;
 };
 
-const areElementsEqual = function (element1, element2) {
-  if (areLengthsUndefined(element1, element2)) {
-    return element1 === element2;
+const areEqual = function (element1, element2) {
+  if (Array.isArray(element1) && Array.isArray(element2)) {
+    return areArraysEqual(element1, element2);
   }
 
-  return areArraysEqual(element1, element2);
+  return element1 === element2;
 };
 
-const isSubset = function (set, subset) {
+const isPresent = function (subset, set) {
   for (let index = 0; index < set.length; index++) {
-    if (areElementsEqual(set[index], subset)) {
+    if (areEqual(set[index], subset)) {
       return true;
     }
   }
@@ -38,7 +34,7 @@ const getUniqueElements = function (set) {
   const uniqueElements = [];
 
   for (let index = 0; index < set.length; index++) {
-    if (!isSubset(uniqueElements, set[index])) {
+    if (!isPresent(set[index], uniqueElements)) {
       uniqueElements.push(set[index]);
     }
   }
@@ -46,24 +42,28 @@ const getUniqueElements = function (set) {
   return uniqueElements;
 };
 
-const uniqueSubsets = function (set) {
+const groupElement = function (element, set) {
+  const subset = [];
+
+  for (let position = 0; position < set.length; position++) {
+    if (areEqual(element, set[position])) {
+      subset.push(set[position]);
+    }
+  }
+  return subset;
+};
+
+const groupSameElements = function (set) {
   const uniqueElements = getUniqueElements(set);
   const subsets = [];
 
   for (let index = 0; index < uniqueElements.length; index++) {
-    const subset = [];
-
-    for (let j = 0; j < set.length; j++) {
-      if (areElementsEqual(uniqueElements[index], set[j])) {
-        subset.push(set[j]);
-      }
-    }
-    subsets.push(subset);
+    subsets.push(groupElement(uniqueElements[index], set));
   }
 
   return subsets;
 };
 
-console.log(uniqueSubsets([1, 2, 1]));
-console.log(uniqueSubsets([1, 2, 3, 1, 2, 4]));
-console.log(uniqueSubsets([[1, 1], 1, [1, 1], 1]));
+console.log(groupSameElements([1, 2, 1])); //[[1,1], 2]
+console.log(groupSameElements([1, 2, 3, 1, 2, 4])); //[[1,1], [2,2], [3], [4]]
+console.log(groupSameElements([[1, 1], 1, [1, 1], 1])); //[ [ [ 1, 1 ], [ 1, 1 ] ], [ 1, 1 ] ]
